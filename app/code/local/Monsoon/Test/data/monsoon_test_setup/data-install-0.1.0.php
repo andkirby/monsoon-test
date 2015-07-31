9ca70005fa690aea87753aec35d168408be91a38
Andrew Roslik <andkirby@gmail.com>
2015-07-31 21:37:58 +0300

    - Added attibute to category.

--- app/code/local/Monsoon/Test/data/monsoon_test_setup/data-install-0.1.0.php
+++ app/code/local/Monsoon/Test/data/monsoon_test_setup/data-install-0.1.0.php
@@ -1,2 +1,13 @@
 <?php
-/* @var $this Mage_Core_Model_Resource_Setup */
+/* @var $this Mage_Catalog_Model_Resource_Setup */
+$this->addAttribute('catalog_category', 'is_not_link', array(
+    'group'         => 'General Information',
+    'input'         => 'select',
+    'type'          => 'int',
+    'label'         => 'Show category as simple label (not as link)',
+    'source'       => 'eav/entity_attribute_source_boolean',
+    'visible'       => true,
+    'required'      => false,
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'sort_order'    => 200,
+));
