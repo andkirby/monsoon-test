1e748c613de38a928f35b95acbfbc3494c5be259
Andrew Roslik <andkirby@gmail.com>
2015-07-31 21:48:11 +0300

    - Added extra comments.

--- app/code/local/Monsoon/Test/data/monsoon_test_setup/data-install-0.1.0.php
+++ app/code/local/Monsoon/Test/data/monsoon_test_setup/data-install-0.1.0.php
@@ -1,10 +1,13 @@
 <?php
+/**
+ * Added boolean attribute to category to show category in top menu as regular label, i.e. not as a link
+ */
 /* @var $this Mage_Catalog_Model_Resource_Setup */
 $this->addAttribute('catalog_category', 'is_not_link', array(
     'group'         => 'General Information',
     'input'         => 'select',
     'type'          => 'int',
-    'label'         => 'Show category as simple label (not as link)',
+    'label'         => 'Show category as a simple label (not as a link)',
     'source'       => 'eav/entity_attribute_source_boolean',
     'visible'       => true,
     'required'      => false,
